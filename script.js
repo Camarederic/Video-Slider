d529d49a0e599310382ec39fa1e48e2c073dae6d
Camarederic <camarederic@gmail.com>
2026-02-28 18:02:23 +0300

    added code for video files JS

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-'use strict'
+"use strict";
 
+const slides = [...document.querySelectorAll(".slide")];
+
+slides.forEach((slide) => {
+  slide.addEventListener("click", () => {
+    const activeSlide = document.querySelector(".slide.active");
+
+    const activeSlideProps = Object.assign(activeSlide.getBoundingClientRect());
+    const slideProps = slide.getBoundingClientRect();
+
+    activeSlide.style.cssText = `width: ${slideProps.width}px; height: ${slideProps.height}px; top: ${slideProps.top}px; left:${slideProps.left}px`;
+    slide.style.cssText = `width: ${activeSlideProps.width}px; height: ${activeSlideProps.height}px; top: ${activeSlideProps.top}px; left:${activeSlideProps.left}px`;
+    activeSlide.classList.remove("active");
+    slide.classList.add("active");
+  });
+});
